5427958bff28bf67e5e4837d53eb30ad065d6a8f
Luke Clifton <ltclifton@gmail.com>
2017-10-23 15:27:35 +0800

    New tests

--- test/Main.hs
+++ test/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import qualified Data.Aeson as Aeson
 import Hypersphere.Check
 import Hypersphere.Density
+import Hypersphere.Sample
 import Test.QuickCheck.Instances ()
 import Test.Tasty
 import Test.Tasty.HUnit
@@ -21,7 +22,10 @@ roundTripJSON a = Aeson.fromJSON (Aeson.toJSON a) == Aeson.Success a
 
 nearlyEqual :: Double -> Double -> Bool
 nearlyEqual a b
-    | (a * 1.01 > b) && (b * 1.01 > a) = True
+    -- If they are within 1.01 of each other
+    | (a * 1.01 >= b) && (b * 1.01 >= a) = True
+    -- Or if they are both with 0.01 of 0.
+    | (abs a < 0.01) && (abs b < 0.01) = True
     | otherwise = False
 
 unitTests :: TestTree
@@ -35,7 +39,9 @@ unitTests = testGroup "Unit Tests"
 qcProps :: TestTree
 qcProps = testGroup "(QuickCheck)"
     [ QC.testProperty "Reason JSON" (roundTripJSON :: Reason -> Bool)
-    , QC.testProperty "Probability distribution sums to 1" (nearlyEqual 1.0 . integrate . kde)
+    , QC.testProperty "Probability distribution sums to 1" $ nearlyEqual 1.0 . integrate . kde
+    , QC.testProperty "Rounding is divisible by target" $
+        \(NonZero r) t -> let n = (rounding r t)/r in nearlyEqual 0 (n - fromIntegral (round n))
     ]
 
 --------------------------------------------------
