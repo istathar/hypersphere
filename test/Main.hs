a3a0a5d6a8eef94a418c67004bd1206d24e499b5
Luke Clifton <ltclifton@gmail.com>
2017-10-23 08:37:37 +0800

    Added Density

--- test/Main.hs
+++ test/Main.hs
@@ -2,8 +2,10 @@ module Main where
 
 import qualified Data.Aeson as Aeson
 import Hypersphere.Check
+import Hypersphere.Density
 import Test.QuickCheck.Instances ()
 import Test.Tasty
+import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck as QC
 
 main = defaultMain tests
@@ -12,14 +14,28 @@ tests :: TestTree
 tests = testGroup "Tests" [properties]
 
 properties :: TestTree
-properties = testGroup "Properties" [qcProps]
+properties = testGroup "Properties" [unitTests,qcProps]
 
 roundTripJSON :: (Eq a, Aeson.FromJSON a, Aeson.ToJSON a) => a -> Bool
 roundTripJSON a = Aeson.fromJSON (Aeson.toJSON a) == Aeson.Success a
 
+nearlyEqual :: Double -> Double -> Bool
+nearlyEqual a b
+    | (a * 1.01 > b) && (b * 1.01 > a) = True
+    | otherwise = False
+
+unitTests :: TestTree
+unitTests = testGroup "Unit Tests"
+    [ testCase "Trapezoidal Area (square)" $ trapezoidalArea 1 1 1 @?= 1
+    , testCase "Trapezoidal Area (wide rect)" $ trapezoidalArea 2 1 1 @?= 2
+    , testCase "Trapezoidal Area (tall rect)" $ trapezoidalArea 1 2 2 @?= 2
+    , testCase "Trapezoidal Area (trapezoid)" $ trapezoidalArea 2 1 0.5 @?= 1.5
+    ]
+
 qcProps :: TestTree
 qcProps = testGroup "(QuickCheck)"
     [ QC.testProperty "Reason JSON" (roundTripJSON :: Reason -> Bool)
+    , QC.testProperty "Probability distribution sums to 1" (nearlyEqual 1.0 . integrate . kde)
     ]
 
 --------------------------------------------------
